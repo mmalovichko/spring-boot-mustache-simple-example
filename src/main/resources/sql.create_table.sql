87ea57806c64dd27f787aba9bb58d946d4057639
mmalovichko <malovichko.maksym@gmail.com>
2019-09-22 22:35:25 -0400

    Added sql create table

---
+++ src/main/resources/sql.create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `sweater` (
+  `id` int(11) NOT NULL,
+  `text` varchar(45) DEFAULT NULL,
+  `tag` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
